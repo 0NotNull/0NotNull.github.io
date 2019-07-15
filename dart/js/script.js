9b1c0da936ee073179d721c6046dadbea2b31789
Ваше Имя <mister.dima3@mail.ru>
2019-07-15 17:36:47 +0300

    update dart link

--- dart/js/script.js
+++ dart/js/script.js
@@ -1,6 +1,8 @@
 let tabsContainer = document.getElementById('service-tabs');
 
 tabsContainer.addEventListener('click', (Event) => {
-  Event.srcElement.nextElementSibling.classList.toggle("is-hidden");
-  Event.srcElement.classList.toggle("tab-title-open")
+  if (Event.target.classList[0] === "tab-title") {
+    Event.srcElement.nextElementSibling.classList.toggle("is-hidden");
+    Event.srcElement.classList.toggle("tab-title-open");
+  }
 });
